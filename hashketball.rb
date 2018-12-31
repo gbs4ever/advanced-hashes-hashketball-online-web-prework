85f544bdd802789e13a15c3f35917492698cec8b
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-31 03:51:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ end
 
 
 def team_colors(team)
-  game_hash.each do |team, team_data|
+  game_hash.each do |team,team_data|
 
 if team_data[:team_name]==team
 return team_data[:team_name]
@@ -141,7 +141,7 @@ end
 end
 
 def team_names
-game_hash.each do | team, team_data|
+game_hash.each do | team,team_data|
   #binding.pry
   team_data.each  do |team_name|
     team name
