fc9fa281efd99f629d7985f4af35916c300e5bc6
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-31 04:27:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -150,10 +150,10 @@ def player_numbers(team_name)
   game_hash.each do |location, team_data|
     if team_data[:team_name]  ==  team_name
   team_data[:players].each do |players,name|
-    binding.pry
+  
     array<< name[:number]
       end
       end
+end
 return array
-      end
 end
